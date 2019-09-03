108202dc50fa6d692e343b58c8f17d9a083cf390
unknown <dheeraj1990kk@gmail.com>
2019-09-03 18:26:07 +0530

    final version 2

--- src/main/java/com/rest/app/UserExample.java
+++ src/main/java/com/rest/app/UserExample.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserExample {
-
+// entry point
 	public static void main(String[] args) {
 		SpringApplication.run(UserExample.class, args);
 	}
