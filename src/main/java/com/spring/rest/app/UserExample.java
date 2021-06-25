473a8c4f7e31e3fd2995ce7f3ea14f00b0eedafe
Dheeraj <dheeraj1990kk@gmail.com>
2021-06-25 16:23:28 +0530

    test fo intellij

--- src/main/java/com/spring/rest/app/UserExample.java
+++ src/main/java/com/spring/rest/app/UserExample.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserExample {
-// main application
+// main application test
 	public static void main(String[] args) {
 		SpringApplication.run(UserExample.class, args);
 	}
